02ee00837585ac7d86ad6ad071d0d14c417ea903
Cody Gordon <codyjg10@gmail.com>
2019-12-23 07:07:00 -0500

    added qr code to coupons

--- CouponBuddy/ViewModels/Coupon/CouponScreenViewModel.cs
+++ CouponBuddy/ViewModels/Coupon/CouponScreenViewModel.cs
@@ -4,19 +4,37 @@ using CouponBuddy.Entities;
 using CouponBuddy.Views.Coupon;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using CouponBuddy.Util;
+using CouponBuddy.ViewModels;
+using QRCoder;
 
 namespace BrochureBuddy.ViewModels.Coupon
 {
-    public class CouponScreenViewModel
+    public class CouponScreenViewModel : ViewModel
     {
         public VendorCoupon Coupon { get; set; }
+
+        private ImageSource _qrCodeImage;
+
+        public ImageSource QrCodeImage
+        {
+            get { return _qrCodeImage; }
+            set
+            {
+                _qrCodeImage = value;
+                OnPropertyChanged(nameof(_qrCodeImage));
+            }
+        }
 
         public CouponScreenViewModel(VendorCoupon coupon)
         {
             Coupon = coupon;
+            LoadQrImage();
         }
 
         public void SendEmail(string email)
@@ -36,5 +54,20 @@ namespace BrochureBuddy.ViewModels.Coupon
         {
             TextSender.SendText(Coupon, number);
         }
+
+        private void LoadQrImage()
+        {
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                string baseUrl = CouponBuddy.Properties.Resources.BASE_URL;
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(baseUrl + "/coupons?id=" + Coupon.Id, QRCodeGenerator.ECCLevel.Q);
+                using (QRCode qrCode = new QRCode(qrCodeData))
+                {
+                    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                    QrCodeImage = BitmapImageLoader.ToBitmapImage(qrCodeImage) as ImageSource;
+                }
+            }
+        }
     }
+
 }
