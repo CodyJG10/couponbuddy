75e9de8d06280142db5824e492b3f373edfc0968
Cody Gordon <codyjg10@gmail.com>
2020-09-29 23:48:18 -0400

    updates

--- CouponBuddy/Properties/Settings.Designer.cs
+++ CouponBuddy/Properties/Settings.Designer.cs
@@ -85,7 +85,7 @@ namespace CouponBuddy.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool FIRST_LAUNCH {
             get {
                 return ((bool)(this["FIRST_LAUNCH"]));
